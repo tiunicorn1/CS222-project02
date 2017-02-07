b9079863242104d9553d37bd94e85f21c39d6a46
Tiara Dalton <s1419209@student.mcckc.edu>
2017-02-06 18:36:04 -0600

    Added all files

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by s1419209 on 2/6/2017.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int i = 42;
+
+        System.out.println("i = " + i);
+    }
+}
